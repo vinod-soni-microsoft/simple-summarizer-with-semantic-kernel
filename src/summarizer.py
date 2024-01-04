f16fa5054fb9e429d08ea696730597b3d7ae391d
Vinod Soni <117770765+vinod-soni-microsoft@users.noreply.github.com>
2024-01-04 05:17:13 +0000

    some changes into my code

--- src/summarizer.py
+++ src/summarizer.py
@@ -2,3 +2,12 @@ import os
 import openai
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+kernel = sk.Kernel()
+
+deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
+kernel.add_text_completion_service("dv", OpenAIChatCompletion(deployment, endpoint, api_key))
+
+prompt = kernel.create_semantic_function("""hello world""")
+
+print(prompt())
